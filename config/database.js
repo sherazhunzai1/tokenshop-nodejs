55217db3d7ecebca74cb0fc6b666985ce894c8a4
Asad Ali Asim <asadaliasim909@gmail.com>
2024-05-23 13:43:23 +0500

    fix: db connection

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: "190.92.150.239",
-  user: "legaland_sheraz",
-  password: "Sherazali11.",
-  database: "legaland_tokenshop",
+  host: '24.199.82.205',
+  user: 'sheraz',
+  password: 'Sherazali11.',
+  database: 'tokenshop',
 });
 module.exports = pool.promise();
